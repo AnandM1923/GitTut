09bb475aae88ef37f404812c437517c3218975e0
Anand <anand@gmail.com>
2023-12-04 21:17:23 +0530

    the second commit

---
+++ src/test/java/testPackages/class02.java
@@ -0,0 +1,11 @@
+package testPackages;
+
+public class class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The second class");
+
+	}
+
+}
